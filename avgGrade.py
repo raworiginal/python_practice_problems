c78a16f31922f082fdd8959b5485e3bb5c1c4295
raworiginal <randall.a.wolfe@gmail.com>
2025-09-04 20:06:09 -0700

    tested to make sure they all work

--- avgGrade.py
+++ avgGrade.py
@@ -1,11 +1,12 @@
 def avgGrade(grades):
-    pass
+  pass
+
 
 if __name__ == "__main__":
-    grades1 = [52, 59, 81, 74, 72, 88, 76, 71, 99, 95]
-    grades2 = [61, 92, 63, 62, 76, 89, 59, 96, 61, 78]
-    grades3 = [98, 62, 54, 96, 85, 89, 68, 51, 79, 89]
+  grades1 = [52, 59, 81, 74, 72, 88, 76, 71, 99, 95]
+  grades2 = [61, 92, 63, 62, 76, 89, 59, 96, 61, 78]
+  grades3 = [98, 62, 54, 96, 85, 89, 68, 51, 79, 89]
 
-    print(avgGrade(grades1))
-    print(avgGrade(grades2))
-    print(avgGrade(grades3))
+  print(avgGrade(grades1))
+  print(avgGrade(grades2))
+  print(avgGrade(grades3))
--- decimalToBinary.py
+++ decimalToBinary.py
@@ -1,8 +1,9 @@
 def decimalToBinary(integer):
-    pass
+  pass
+
 
 if __name__ == "__main__":
-    print(decimalToBinary(15))
-    print(decimalToBinary(9))
-    print(decimalToBinary(21))
-    print(decimalToBinary(5))
+  print(decimalToBinary(15))
+  print(decimalToBinary(9))
+  print(decimalToBinary(21))
+  print(decimalToBinary(5))
