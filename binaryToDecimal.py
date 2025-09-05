05cb0a91a7759228257c3625734c72ddf91883fe
raworiginal <randall.a.wolfe@gmail.com>
2025-09-04 20:06:09 -0700

    tested to make sure they all work

--- binaryToDecimal.py
+++ binaryToDecimal.py
@@ -1,9 +1,10 @@
 def binaryToDecimal(binaryStr):
-    pass
+  pass
+
 
 if __name__ == "__main__":
-    print(binaryToDecimal("1010"))
-    print(binaryToDecimal("11"))
-    print(binaryToDecimal("100"))
-    print(binaryToDecimal("0"))
-    print(binaryToDecimal("1111"))
+  print(binaryToDecimal("1010"))
+  print(binaryToDecimal("11"))
+  print(binaryToDecimal("100"))
+  print(binaryToDecimal("0"))
+  print(binaryToDecimal("1111"))
--- concatFirstAndLastNames.py
+++ concatFirstAndLastNames.py
@@ -1,8 +1,9 @@
-def concatFirstAndLastNames(firstNames,lastNames):
-    pass
+def concatFirstAndLastNames(firstNames, lastNames):
+  pass
 
 
 if __name__ == "__main__":
-    first_names = ['Carol','Reed','Steve','Natasha','Clint','Wanda','Tony']
-    last_names = ['Danvers','Richards','Rogers','Romanoff','Barton','Maximoff','Stark']
-    print(concatFirstAndLastNames(first_names,last_names))
+  first_names = ['Carol', 'Reed', 'Steve', 'Natasha', 'Clint', 'Wanda', 'Tony']
+  last_names = ['Danvers', 'Richards', 'Rogers',
+                'Romanoff', 'Barton', 'Maximoff', 'Stark']
+  print(concatFirstAndLastNames(first_names, last_names))
